d19e2ab0425c9354377858c9320371bfdf8a2e53
tarabacha <56684364+tarabacha@users.noreply.github.com>
2021-10-31 21:22:02 +0530

    Update asgi.py
    Adding the security patch of dotenv

--- notification/notification/asgi.py
+++ notification/notification/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from dotenv import load_dotenv
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notification.settings')
 
 application = get_asgi_application()
